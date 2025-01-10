25e3ab4158f4a54d326cfdf2a77aef8b1fdc4373
Wyatt Radkiewicz <wyattwradkiewicz@gmail.com>
2025-01-10 10:35:44 -0700

    A little update

--- components/techs.tsx
+++ components/techs.tsx
@@ -4,8 +4,8 @@ import { motion } from 'framer-motion';
 
 function Tech({src, alt, ...props}: any) {
   return <motion.img
-    whileHover={{ scale: 1.2, rotate: 4, translateY: -20 }}
-    whileTap={{ scale: 0.8, rotate: -4 }}
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
     src={src}
     alt={alt}
     width={768}
@@ -19,14 +19,19 @@ export default function Techs() {
   return (
     <motion.div
       initial={{ borderRadius: "1rem" }}
-      whileHover={{ scale: 1.05, borderRadius: "8rem" }}
-      transition={{ duration: 0.8, type: "spring" }}
+      //whileHover={{ scale: 1.05, borderRadius: "8rem" }}
+      //transition={{ duration: 0.8, type: "spring" }}
       className={styles.container}>
       <Tech src="/images/techs/c.png" alt="C"></Tech>
+      <Tech src="/images/techs/c++.png" alt="C++"></Tech>
       <Tech src="/images/techs/nextjs.png" alt="NextJS"></Tech>
       <Tech src="/images/techs/react.png" alt="React"></Tech>
       <Tech src="/images/techs/rust.png" alt="Rust"></Tech>
       <Tech src="/images/techs/ts.png" alt="TS"></Tech>
+      <Tech src="/images/techs/zig.png" alt="Zig"></Tech>
+      <Tech src="/images/techs/linux.png" alt="Linux"></Tech>
+      <Tech src="/images/techs/haskell.png" alt="Haskell"></Tech>
+      <Tech src="/images/techs/opengl.png" alt="OpenGL"></Tech>
       <Tech src="/images/techs/webgl.png" alt="WebGL"></Tech>
     </motion.div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -26,25 +26,32 @@ export default function Home() {
 
       <div className={styles.scrollSpace}>
         <Section boxId="1">
-          <h2 className={`${ubuntu_mono.className}`}>Hello!</h2>
-          <p className={`${ubuntu_mono.className} ${styles.sectionBody}`}>I am 18 years old and a senior at Timberlake High School. I&apos;ve been programming since I was in 4th grade and have ammased a wealth of knowledge and practical skills in coding. I am fluent in C, C++, Rust, Javascript, Typescript, X86 Assembly (Intel), and decent at many other languages. I&apos;m currently learning and quickly picking up on web technologies like React, NextJS, and others. Outside of coding, I am a student of ApogeeStrong led by Matt Beaudreau and Tim Kennedy and have also gotten Eagle Rank in Boy Scouts. My hobbies are coding and bodybuilding.</p>
+          <h2 className={`${ubuntu_mono.className}`}>About Me</h2>
+          <p className={`${ubuntu_mono.className} ${styles.sectionBody}`}>I love to code in my free time and loved working with my team in my previous jobs. I am positive and enthusiastic, work well in a team, and try my hardest with my work and projects. I enjoy both the creative and programming side of projects. I like to write clean fast code and my past projects reflect this.</p>
           <Techs></Techs>
           <div className={styles.contactInfo}>
             <h4 className={ubuntu_mono.className}>wyattwradkiewicz@gmail.com</h4>
             <h4 className={ubuntu_mono.className}>630-947-4984</h4>
-            <h4 className={ubuntu_mono.className}>https://www.youtube.com/@wyattradkiewicz1127</h4>
-          </div>
-          <div className={styles.profilePicContainer}>
-            <h3 className={ubuntu_mono.className}>Hi!</h3>
-            <Image
-              src="/images/profile.jpg"
-              width="144"
-              height="144"
-              alt="A picture of me"
-            />
+            <h4 className={ubuntu_mono.className}>https://github.com/wyatt-radkiewicz</h4>
           </div>
         </Section>
         <Section boxId="2">
+          <ProjectCarosel>
+            <Project imgSrc="/images/projects/scummvm1.png">
+              <h2 className={ubuntu_mono.className}>Google Summer of Code 2023 - Optimizing ScummVM Renderer</h2>
+              <p className={ubuntu_mono.className}>So originally, the goals for the project were to optimize the pixel blending code in the rendering code for the AGS game engine in ScummVM. The problem was: I completed that goal about half way through the coding period. So me and my mentors talked and what I did after was optimize the rendering code that most other engines in ScummVM use. I used SIMD cpu extensions to net a pretty huge performance gain.</p>
+            </Project>
+            <Project imgSrc="/images/projects/scummvm2.png">
+              <h2 className={ubuntu_mono.className}>Renderer speed ups</h2>
+              <p className={ubuntu_mono.className}>In the AGS renderer, it got a 5x improvement all around and a 14x improvement in the best scenarios. In the global rendering code for all engines to use it got a 2x improvement all around. Here are the speed up results.</p>
+            </Project>
+            <Project imgSrc="/images/projects/scummvm3.png">
+              <h2 className={ubuntu_mono.className}>Some bugs along the way</h2>
+              <p className={ubuntu_mono.className}>I’d like to thank Google Summer of Code 2023, ScummVM for the opportunity to work on a project like this and learn so much. And I’d like to thank my mentors for helping me when I was stuck, and teaching me how to work in a team.</p>
+            </Project>
+          </ProjectCarosel>
+        </Section>
+        <Section boxId="3">
           <ProjectCarosel>
             <Project imgSrc="/images/projects/cnm1.jpg">
               <h2 className={ubuntu_mono.className}>One of my games: &apos;CNM ONLINE&apos;</h2>
@@ -75,7 +82,7 @@ export default function Home() {
             </Project>
           </ProjectCarosel>
         </Section>
-        <Section boxId="3">
+        <Section boxId="4">
           <ProjectCarosel>
             <Project imgSrc="/images/projects/smwo1.jpg">
               <h2 className={ubuntu_mono.className}>Super Mario War: Online</h2>
@@ -95,34 +102,21 @@ export default function Home() {
             </Project>
           </ProjectCarosel>
         </Section>
-        <Section boxId="4">
-          <ProjectCarosel>
-            <Project imgSrc="/images/projects/fishyman.jpg">
-              <h2 className={ubuntu_mono.className}>Fishy Man 2</h2>
-              <p className={ubuntu_mono.className}>A short project I made with a friend. Created a very smooth rendering and collision system. Learned the most about collision resolution and trying to squash memory bugs (This was programmed in C).</p>
-              <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-                <a className={`${ubuntu_mono.className} ${styles.link}`} href="https://github.com/wyatt-radkiewicz/fishy-man2">{`<Source Code />`}</a>
-              </div>
-            </Project>
-            <Project imgSrc="/images/projects/ffo.jpg">
-              <h2 className={ubuntu_mono.className}>Food Fight Online</h2>
-              <p className={ubuntu_mono.className}>This was my first test of making a multiplayer game. Its called &apos;Food Fight Online&apos; and was written in C++ with SFML.</p>
-              <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-                <a className={`${ubuntu_mono.className} ${styles.link}`} href="https://github.com/wyatt-radkiewicz/food-fight-online">{`<Source Code />`}</a>
-              </div>
-            </Project>
-            <Project imgSrc="/images/projects/grs-p.jpg">
-              <h2 className={ubuntu_mono.className}>Grass Protector</h2>
-              <p className={ubuntu_mono.className}>One of the first games ever that I made in C/C++ with SFML.</p>
-            </Project>
-          </ProjectCarosel>
-        </Section>
         <Section boxId="5">
           <h2 className={ubuntu_mono.className}>My Blog</h2>
+          <a href="https://blogs.scummvm.org/eklipsed/#site-header">
+            <Image className={styles.blogPortal}
+              src="/images/scummvm_blog.jpg"
+              width="386"
+              height="386"
+              alt="Scummvm blog"
+            />
+          </a>
           <div className={styles.contactInfo}>
             <h4 className={ubuntu_mono.className}>wyattwradkiewicz@gmail.com</h4>
             <h4 className={ubuntu_mono.className}>630-947-4984</h4>
-            <h4 className={ubuntu_mono.className}>https://www.youtube.com/@wyattradkiewicz1127</h4>
+            <h4 className={ubuntu_mono.className}>https://github.com/wyatt-radkiewicz</h4>
+            <h4 className={ubuntu_mono.className}>https://blogs.scummvm.org/eklipsed/#site-header</h4>
           </div>
         </Section>
       </div>
@@ -132,8 +126,8 @@ export default function Home() {
         <TermElement type="cube" starty={sectionPosChars(2, height) - height / 1.25 - 1} endy={sectionPosChars(3, height) - height / 1.25} />
         <TermElement type="matrix" amount="4" speed="20" starty="0" endy={sectionPosChars(2, height) - height / 1.25} />
         <TermElement type="matrix" amount="4" speed="20" starty={sectionPosChars(5, height) - height} />
-        <TermElement type="string" scroll="true" font="Standard" slide="0.6 0.5" value="Hi, I&apos;m" x="5" y="4" />
-        <TermElement type="string" scroll="true" font="Standard" cursor="true" value="Wyatt" slide="0.8 1.0"  x="5" y="10" />
+        <TermElement type="string" scroll="true" font="Standard" slide="0.6 0.5" value="Wyatt" x="3" y="4" />
+        <TermElement type="string" scroll="true" font="Standard" cursor="true" value="Portfolio" slide="0.8 1.0"  x="3" y="10" />
         <TermElement type="string" scroll="true" align="c" value={`\n Scroll For More! \n`} slide="1.1 0.25"  x={width / 2} y={height - 10} />
         <TermElement type="string" scroll="true" font="Mini" align="c" value="About Me" slide="0 0.75" x={Math.floor(width / 2)} y={sectionPosChars(1, height) - 9} />
         <TermElement type="string" scroll="true" font="Mini" align="c" value="Projects" slide="0 0.75" x={Math.floor(width / 2)} y={sectionPosChars(2, height) - 9} />
